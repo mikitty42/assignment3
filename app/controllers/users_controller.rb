95274204e0a4694ec63b235e8db2352415f08046
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-08-25 19:07:32 +0900

    Implementing the User details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,23 @@ class UsersController < ApplicationController
     end
     
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user.id)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users,only: [:new,:create]
+  resources :users,only: [:new,:create,:show]
+  resources :sessions,only: [:new,:create,:destroy]
   resources :pictures do
       collection do
           post :confirm
