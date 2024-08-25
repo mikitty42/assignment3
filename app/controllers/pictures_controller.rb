97823554f5b2213d22a05329911bbe64d953ff75
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-08-25 14:39:37 +0900

    Implemented new and create actions for picture controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,11 +7,37 @@ class PicturesController < ApplicationController
   end
   
   def create
+      @picture = Picture.new(picture_patams)
+      if params[:back]
+          render :new, status: :unprocessable_entity
+      else
+          if @picture.save
+              redirect_to picture_path,notice: 'Pictureを登録しました'
+          else
+              render :new, status: :unprocessable_entity
+          end
+      end
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def confirm
+  end
+  
+  
+  private
+  
+  def picture_patams
+      params.require(:picture).permit(:content,:image,:image_cache)
   end
 end
